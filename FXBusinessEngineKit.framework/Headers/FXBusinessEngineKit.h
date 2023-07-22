072eae585709522954a091d60f494faba439359f
yangfan <DBasdyangfan@outlook.com>
2023-07-22 14:29:08 +0800

    创建空文件

---
+++ FXBusinessEngineKit.framework/Headers/FXBusinessEngineKit.h
@@ -0,0 +1,18 @@
+//
+//  FXBusinessEngineKit.h
+//  FXBusinessEngineKit
+//
+//  Created by 杨帆 on 2023/7/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FXBusinessEngineKit.
+FOUNDATION_EXPORT double FXBusinessEngineKitVersionNumber;
+
+//! Project version string for FXBusinessEngineKit.
+FOUNDATION_EXPORT const unsigned char FXBusinessEngineKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FXBusinessEngineKit/PublicHeader.h>
+
+
